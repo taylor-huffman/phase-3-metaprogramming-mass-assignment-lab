cc1296d10dd6d9f5d1152ef44618b62332457355
Taylor Huffman <taylor@crucialartists.com>
2022-05-17 15:43:37 -0500

    Complete assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  # your code here
+  
+  def initialize(hash)
+    hash.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
 end
